7199d04b34e06e75e0ac4758d1e19d93b7563e88
Vaisakh Murali <mvaisakh@statixos.com>
2022-10-28 14:22:24 +0100

    cezanne: Kang Vibrator service from cannon
    Change-Id: I1d78c51175efb4a85ba466ac6a49369a3917f21e

--- device.mk
+++ device.mk
@@ -221,5 +221,9 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/wifi/wpa_supplicant.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/wpa_supplicant.conf \
     $(LOCAL_PATH)/configs/wifi/wpa_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/wpa_supplicant_overlay.conf
 
+# Vibrator
+PRODUCT_PACKAGES += \
+    android.hardware.vibrator-service.cezanne
+
 # Inherit the proprietary files
 $(call inherit-product, vendor/xiaomi/cezanne/cezanne-vendor.mk)
