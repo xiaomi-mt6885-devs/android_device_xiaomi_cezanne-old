f86b03737a1d8c2504eb9939276e6c04d10f951f
bengris32 <bengris32@protonmail.ch>
2023-01-05 14:44:08 +0100

    cezanne: Import WiFi configs from stock
    Signed-off-by: bengris32 <bengris32@protonmail.ch>
    Change-Id: Ib5677f2875cec67662d19417cba81a92f388dd3e

--- device.mk
+++ device.mk
@@ -136,5 +136,11 @@ PRODUCT_SOONG_NAMESPACES += \
     hardware/mediatek \
     hardware/xiaomi
 
+# Wi-Fi
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/wifi/p2p_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/p2p_supplicant_overlay.conf \
+    $(LOCAL_PATH)/configs/wifi/wpa_supplicant.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/wpa_supplicant.conf \
+    $(LOCAL_PATH)/configs/wifi/wpa_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/wpa_supplicant_overlay.conf
+
 # Inherit the proprietary files
 $(call inherit-product, vendor/xiaomi/cezanne/cezanne-vendor.mk)
